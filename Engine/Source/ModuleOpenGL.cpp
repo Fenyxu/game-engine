dbeca0befaa1251ccc02fc4fda5ba2c166e535dd
David Bardina <bardina.dev@gmail.com>
2024-01-18 00:42:09 +0100

    Add window creation and glew lib.

--- Engine/Source/ModuleOpenGL.cpp
+++ Engine/Source/ModuleOpenGL.cpp
@@ -3,6 +3,7 @@
 #include "ModuleOpenGL.h"
 #include "ModuleWindow.h"
 #include "SDL.h"
+#include "GL/glew.h"
 
 ModuleOpenGL::ModuleOpenGL()
 {
@@ -17,12 +18,44 @@ ModuleOpenGL::~ModuleOpenGL()
 bool ModuleOpenGL::Init()
 {
 	LOG("Creating Renderer context");
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4); // desired version
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); // we want a double buffer
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24); // we want to have a depth buffer with 24 bits
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8); // we want to have a stencil buffer with 8 bits
+
+	context = SDL_GL_CreateContext(App->GetWindow()->window);
+
+	GLenum err = glewInit();
+	// … check for errors
+	LOG("Using Glew %s", glewGetString(GLEW_VERSION));
+	// Should be 2.0
+
+	LOG("Vendor: %s", glGetString(GL_VENDOR));
+	LOG("Renderer: %s", glGetString(GL_RENDERER));
+	LOG("OpenGL version supported %s", glGetString(GL_VERSION));
+	LOG("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	glEnable(GL_DEPTH_TEST); // Enable depth test
+	glEnable(GL_CULL_FACE); // Enable cull backward faces
+	glFrontFace(GL_CCW); // Front faces will be counter clockwise
 
 	return true;
 }
 
 update_status ModuleOpenGL::PreUpdate()
 {
+	GLsizei width{ 0 };
+	GLsizei height{ 0 };
+
+	SDL_GetWindowSize(App->GetWindow()->window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	glClearColor(0.0f, 0.0, 1.0f, 1.0f);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 	return UPDATE_CONTINUE;
 }
 
@@ -35,6 +68,8 @@ update_status ModuleOpenGL::Update()
 
 update_status ModuleOpenGL::PostUpdate()
 {
+	SDL_GL_SwapWindow(App->GetWindow()->window);
+
 	return UPDATE_CONTINUE;
 }
 
@@ -42,7 +77,7 @@ update_status ModuleOpenGL::PostUpdate()
 bool ModuleOpenGL::CleanUp()
 {
 	LOG("Destroying renderer");
-
+	SDL_GL_DeleteContext(context);
 	//Destroy window
 
 	return true;
